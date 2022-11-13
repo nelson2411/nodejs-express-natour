25569eb518bb9ebf75615b947d862f56548cc42f
Nelson Rosales <nelson.rosales83@gmail.com>
2022-11-13 13:48:40 +0100

    feat: server file

--- app.js
+++ app.js
@@ -27,12 +27,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const port = 3000;
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 3) Start the server
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
