8d9d450d65cdc825d922e98243643dd0f3c003ba
Nelson Rosales <nelson.rosales83@gmail.com>
2022-11-19 11:31:10 +0100

    feat: mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 const app = require('./app');
 
