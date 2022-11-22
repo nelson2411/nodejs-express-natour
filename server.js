07c4033a2fd24806e40da82c55ce9e8bd6bb6985
Nelson Rosales <nelson.rosales83@gmail.com>
2022-11-22 20:06:30 +0100

    feat: server

--- server.js
+++ server.js
@@ -19,6 +19,39 @@ mongoose
     console.log('DB connection successful!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.1,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'Atecozol',
+  rating: 4.3,
+  price: 3,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR:', err);
+  });
+
 const app = require('./app');
 
 // 3) Start the server
