de03d9e57393ce7817fc65e3155a51b3bb18a33c
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-19 12:23:28 +0100

    feat: refactoring route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,9 +7,14 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getAllTours = async (req, res) => {
-  try {
-    const queryObj = { ...req.query };
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
@@ -17,42 +22,96 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj); // Here we convert the query object to a string
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // Here we replace the operators with the $ sign
     console.log(JSON.parse(queryStr));
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
 
-    let query = Tour.find(JSON.parse(queryStr));
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy); // Here we sort the query by the sort parameter
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1; // Here we convert the page parameter to a number
+    const limit = this.queryString.limit * 1 || 100; // Here we convert the limit parameter to a number
+    const skip = (page - 1) * limit; // Here we calculate the number of documents to skip
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // const queryObj = { ...req.query };
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // excludedFields.forEach((el) => delete queryObj[el]);
+
+    // // 1B) Advanced filtering
+    // let queryStr = JSON.stringify(queryObj); // Here we convert the query object to a string
+    // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`); // Here we replace the operators with the $ sign
+    // console.log(JSON.parse(queryStr));
+    // // const tours = await Tour.find()
+    // //   .where('duration')
+    // //   .equals(5)
+    // //   .where('difficulty')
+    // //   .equals('easy');
+
+    // let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy); // Here we sort the query by the sort parameter
-    }
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   console.log(sortBy);
+    //   query = query.sort(sortBy); // Here we sort the query by the sort parameter
+    // }
 
     // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    // } else {
+    //   query = query.select('-__v');
+    // }
 
     // 4) Pagination
-    const page = req.query.page * 1 || 1; // Here we convert the page parameter to a number
-    const limit = req.query.limit * 1 || 100; // Here we convert the limit parameter to a number
-    const skip = (page - 1) * limit; // Here we calculate the number of documents to skip
-    query = query.skip(skip).limit(limit);
+    // const page = req.query.page * 1 || 1; // Here we convert the page parameter to a number
+    // const limit = req.query.limit * 1 || 100; // Here we convert the limit parameter to a number
+    // const skip = (page - 1) * limit; // Here we calculate the number of documents to skip
+    // query = query.skip(skip).limit(limit);
 
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments();
+    //   if (skip >= numTours) throw new Error('This page does not exist');
+    // }
 
     // Execute the query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
