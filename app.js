c2b0a3aeb224e7424d19da966e0cada4fbde80f0
Nelson Rosales <nelson.rosales83@gmail.com>
2022-12-21 11:54:43 +0100

    feat: handling errors

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const { json } = require('express/lib/response');
-const fs = require('fs');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -32,7 +30,19 @@ app.use((req, res, next) => {
   next();
 });
 
+// 3) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Unhandling routes
+
+// app.all is a middleware that handles all HTTP methods
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    // req.originalUrl is the url that the user requested
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
