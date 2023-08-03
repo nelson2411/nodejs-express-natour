eabdd1a188718385b1ea0bb8acd487e23d467a46
Nelson Rosales <nelson.rosales83@gmail.com>
2023-08-03 19:29:45 +0200

    fix: updateuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,15 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    // if the el is not in the allowedFields, then add it to the newObj
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
@@ -27,9 +36,17 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   }
 
   // 2. Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     status: 'success',
+    data: {
+      user: updatedUser,
+    },
   });
 });
 
